005d91dc28b8ebd84ec5683afcda73db3e31c3b7
sai <tharamac2@gmail.com>
2026-06-25 11:41:01 +0530

    feat: map subcontractor/vendor code to vendorCode property on delivery challan instead of siteCode

--- src/app/pages/StoreView.tsx
+++ src/app/pages/StoreView.tsx
@@ -202,16 +202,18 @@ const StoreView = () => {
   const generatePDF = (tool: any, transactionDetails: string, remarks: string, type: 'RECEIPT' | 'DISPATCH' = 'RECEIPT') => {
     let consignee = storeLocation;
     let siteCode: string | undefined;
+    let vendorCode: string | undefined;
 
     if (type === 'DISPATCH') {
       if (transactionDetails === 'subcon_work') {
         consignee = tool.subcontractor_name || 'Sub-Contractor';
-        siteCode = tool.subcontractor_code || undefined;
+        siteCode = tool.current_site || undefined;
+        vendorCode = tool.subcontractor_code || undefined;
       } else if (transactionDetails === 'site_transfer') {
         consignee = tool.current_site || 'Site';
       } else if (transactionDetails === 'scrap_disposal') {
         consignee = tool.subcontractor_name || 'Scrap Dealer';
-        siteCode = tool.subcontractor_code || undefined;
+        vendorCode = tool.subcontractor_code || undefined;
       }
     }
 
@@ -219,6 +221,7 @@ const StoreView = () => {
       type,
       consignee,
       siteCode,
+      vendorCode,
       remarks,
       items: [{
         description: tool.description,
