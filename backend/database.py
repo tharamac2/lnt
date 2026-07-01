3cdea471cf16b4c70d743e7db4f6a189e3bf1706
sai <tharamac2@gmail.com>
2026-07-01 10:47:02 +0530

    chore: update database connection to use MySQL lnt qr code.db

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
 from sqlmodel import create_engine, Session, SQLModel
 
-# Use SQLite instead of MySQL for local testing
-DATABASE_URL = "sqlite:///./qr_tools_db.db"
+# Connect to the local MySQL database
+DATABASE_URL = "mysql+pymysql://root:@localhost/lnt qr code.db"
 
-engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, echo=False)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
